a5163647717d1157ca0afabb2c02f20cb467f4e1
Bohou.Zhang <zhangbohou0519@gmail.com>
2024-06-15 00:29:06 +0800

    [BOMusicMod]工程初始化

---
+++ src/main/java/com/bohou/bomusicmod/BOMusicModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.bohou.bomusicmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BOMusicModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
