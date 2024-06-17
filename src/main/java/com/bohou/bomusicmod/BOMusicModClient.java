9f8c99df25a3788587e1950aa71a3506a09b6a99
Bohou.Zhang <zhangbohou0519@gmail.com>
2024-06-17 23:34:44 +0800

    [BOMusicMod]初始化BOMusicModClient，配置文件中增加描述和制作人员名单

---
+++ src/main/java/com/bohou/bomusicmod/BOMusicModClient.java
@@ -0,0 +1,11 @@
+package com.bohou.bomusicmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BOMusicModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
